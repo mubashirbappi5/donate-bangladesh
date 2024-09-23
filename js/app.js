899c70f0fd917f761592dcc2063bdea93161cf3a
mubashirbappi <mubashirbappi@gmail.com>
2024-09-23 17:34:49 +0600

    card1 full complate

--- js/app.js
+++ js/app.js
@@ -4,6 +4,21 @@ const donationBtn = document.getElementById('donation-btn')
 const navberscroll =document.getElementById('nav-scrol')
 const historySection = document.getElementById('history-section')
 const donationSection = document.getElementById('donation-section')
+const donateNowBtn = document.getElementById('donate-now-btn')
+
+const donatesowDigt = document.getElementById('donate-digit')
+// input value
+function getinputValueByid(id){
+    const inputValue = document.getElementById(id).value
+    const inputNumber = parseFloat(inputValue)
+    return inputNumber
+}
+// text number value function
+function gettextNumberByid(id){
+    const textValue = document.getElementById(id).innerText
+    const number = parseFloat(textValue)
+    return number
+}
 
 
 
@@ -34,4 +49,30 @@ document.getElementById('donation-btn').addEventListener("click",function(){
     historyBtn.classList.remove('bg-primary','border-none','text-black')
     historySection.classList.add('hidden')
     donationSection.classList.remove('hidden')
+})
+
+// donation now btn
+document.getElementById('donate-now-btn').addEventListener("click",function(event){
+    event.preventDefault()
+    const donateAmountInput = getinputValueByid('donate-amount-input');
+    if(isNaN(donateAmountInput)){
+        alert("please provide valid amount number")
+        return
+    }
+   
+    
+     const donateDigitnumber = gettextNumberByid('donate-digit')
+     const mainAmount = gettextNumberByid('main-amount')
+     const availableAmount = mainAmount - donateAmountInput
+     if(availableAmount < donateAmountInput){
+        alert('insufficient balance')
+        return
+    }
+     const donatetotal = donateAmountInput + donateDigitnumber
+     document.getElementById('donate-digit').innerText = donatetotal
+     document.getElementById('main-amount').innerText = availableAmount
+
+     
+    
+
 })
